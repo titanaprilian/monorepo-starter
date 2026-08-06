4048aefaa4ae2e8c3112331298de3dbd26ee68d3
titanaprilian <titanaprilian73@gmail.com>
2026-08-06 14:23:06 +0700

    chore: delete all the test file before migrating to traditional test implementation

--- packages/db/src/run-seeder.test.ts
+++
@@ -1,8 +0,0 @@
-import { describe, expect, test } from "bun:test";
-import { runSeeder } from "./run-seeder";
-
-describe("runSeeder", () => {
-  test("is exported as a function", () => {
-    expect(typeof runSeeder).toBe("function");
-  });
-});
